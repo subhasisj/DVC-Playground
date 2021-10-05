1f65bda1b6e9ba03a33445c51d18cfe33d2356ad
subhasisj <monty.rulz@gmail.com>
2021-10-06 00:54:30 +0530

    1st Commit

---
+++ stage_01.py
@@ -0,0 +1,2 @@
+with open("artifact_from_01.txt", "w+") as f:
+    f.write("This is a sample text again")
---
+++ stage_02.py
@@ -0,0 +1,4 @@
+with open("artifact_from_01.txt","r") as f:
+    text = f.read()
+
+print(f"Text: {text}")
